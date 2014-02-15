5934a909618c8c2518fd0ff1439552e4bd3084c7
mcarlile <mckenzie.carlile@gmail.com>
2014-02-14 23:47:51 -0800

    Alpha 0.4
    New artwork, rolled back artwork to old version then started over.
    Should work now.

---
+++ Assets/Scripts/Corner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Corner : MonoBehaviour
+{
+
+		public GameObject manager;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void OnTriggerStay ()
+		{
+				manager.GetComponent<Manager> ().IncrementScore ();
+
+				
+		}
+}
